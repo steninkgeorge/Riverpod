afddf306ff9b5039fe89e0bbdc03f9bde3b53665
stenin <steninkgeorge007@gmail.com>
2023-06-06 15:37:50 +0530

    you will never know what i changed heheheh

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         // restart instead.
         //
         // This works for code too, not just values: Most code changes can be
-        //blabkavka
+        //
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
