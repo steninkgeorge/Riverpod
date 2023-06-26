0f4cf8217521191d7336896ca0dd19b8f6378c06
stenin <steninkgeorge007@gmail.com>
2023-06-27 01:10:38 +0530

    success

--- lib/main.dart
+++ lib/main.dart
@@ -8,22 +8,20 @@ void main() {
 final nameprovider = Provider((ref) => 'stenin');
 final iconProvdier = StateProvider((ref) => Icons.wb_sunny);
 
-class MyApp extends StatelessWidget {
+final themeProvider = StateProvider<bool>((ref) => true);
+
+class MyApp extends ConsumerWidget {
   MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final iconProvider = ref.watch(iconProvdier);
     return MaterialApp(
         title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          brightness: Brightness.light,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          useMaterial3: true,
-        ),
-        darkTheme: ThemeData.dark(),
-        themeMode: ThemeMode.dark,
+        theme: iconProvider == Icons.wb_sunny
+            ? ThemeData.light()
+            : ThemeData.dark(),
         home: Scaffold(
           body: Center(
             child: Consumer(builder: (context, ref, child) {
@@ -32,11 +30,10 @@ class MyApp extends StatelessWidget {
               return IconButton(
                 icon: Icon(iconData),
                 onPressed: () {
-                  final newIcon = iconData == Icons.wb_sunny
-                      ? Icons.nightlight_round
-                      : Icons.wb_sunny;
-
-                  ref.read(iconProvdier.notifier).state = newIcon;
+                  ref.read(iconProvdier.notifier).state =
+                      iconData == Icons.wb_sunny
+                          ? Icons.nightlight_outlined
+                          : Icons.wb_sunny;
                 },
               );
             }),
