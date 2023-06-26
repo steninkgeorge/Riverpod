a6fb1e8b6e4039dfdbc860524ff7e751f34a0db9
stenin <steninkgeorge007@gmail.com>
2023-06-27 00:39:41 +0530

    riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_/homepage.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
 }
 
 final nameprovider = Provider((ref) => 'stenin');
-final isDarkThemeProvider = Provider<bool>((ref) => false);
+final iconProvdier = StateProvider((ref) => Icons.wb_sunny);
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
@@ -19,8 +18,29 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
+          brightness: Brightness.light,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
           useMaterial3: true,
         ),
-        home: homepage());
+        darkTheme: ThemeData.dark(),
+        themeMode: ThemeMode.dark,
+        home: Scaffold(
+          body: Center(
+            child: Consumer(builder: (context, ref, child) {
+              final iconData = ref.watch(iconProvdier);
+
+              return IconButton(
+                icon: Icon(iconData),
+                onPressed: () {
+                  final newIcon = iconData == Icons.wb_sunny
+                      ? Icons.nightlight_round
+                      : Icons.wb_sunny;
+
+                  ref.read(iconProvdier.notifier).state = newIcon;
+                },
+              );
+            }),
+          ),
+        ));
   }
 }
