f58de7e5254881bfe34403e57003b7a41e66b723
stenin <steninkgeorge007@gmail.com>
2023-07-04 20:41:50 +0530

    riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,20 @@ void main() {
   runApp(ProviderScope(child: MyApp()));
 }
 
-final nameprovider = Provider((ref) => 'stenin');
 final iconProvdier = StateProvider((ref) => Icons.wb_sunny);
 
 final themeProvider = StateProvider<bool>((ref) => true);
+
+final counterController =
+    StateProvider<counterNotifier>((ref) => counterNotifier());
+
+class counterNotifier extends StateNotifier<int> {
+  counterNotifier() : super(0);
+
+  void add() {
+    state = state + 1;
+  }
+}
 
 class MyApp extends ConsumerWidget {
   MyApp({super.key});
@@ -22,21 +32,39 @@ class MyApp extends ConsumerWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: iconProvider == Icons.wb_sunny ? ThemeData.light() : darkTheme,
-        home: Scaffold(
-          body: Center(
-            child: Consumer(builder: (context, ref, child) {
-              final iconData = ref.watch(iconProvdier);
+        home: SafeArea(
+          child: Scaffold(
+            body: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Consumer(builder: (context, ref, child) {
+                      final iconData = ref.watch(iconProvdier);
 
-              return IconButton(
-                icon: Icon(iconData),
-                onPressed: () {
-                  ref.read(iconProvdier.notifier).state =
-                      iconData == Icons.wb_sunny
-                          ? Icons.nightlight_outlined
-                          : Icons.wb_sunny;
-                },
-              );
-            }),
+                      return IconButton(
+                        icon: Icon(iconData),
+                        onPressed: () {
+                          ref.read(iconProvdier.notifier).state =
+                              iconData == Icons.wb_sunny
+                                  ? Icons.nightlight_outlined
+                                  : Icons.wb_sunny;
+                        },
+                      );
+                    }),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Text(ref.watch(counterController).state.toString()),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          ref.read(counterController).add();
+                        },
+                        child: const Text("add"))
+                  ]),
+            ),
           ),
         ));
   }
